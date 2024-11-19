2ac23d57471711976aeaaee3f7c226b99fbdd34b
nyeong <duddnd4457@chosun.ac.kr>
2024-11-20 01:04:44 +0900

    백준 Data Structure stack, queue 문제 업로드

--- algorythm/data_structure/baekjoon_10845.py
+++ algorythm/data_structure/baekjoon_10845.py
@@ -1,8 +1,39 @@
 # https://www.acmicpc.net/problem/10845
 # 큐
-# TODO: Here !
-
-def solution():
-    return None
+import sys
 
 
+def solution(queue, commands):
+    if len(commands) > 1:
+        number = commands[-1]
+        queue.append(number)
+        return
+
+    command = commands[0]
+    print(resolve_command(queue, command))
+    return
+
+
+def resolve_command(queue, command):
+    if command == "pop":
+        return queue.pop(0) if is_present(queue) else -1
+    elif command == "size":
+        return len(queue)
+    elif command == "empty":
+        return 1 if not is_present(queue) else 0
+    elif command == "front":
+        return queue[0] if is_present(queue) else -1
+    elif command == "back":
+        return queue[-1] if is_present(queue) else -1
+
+
+def is_present(queue):
+    return len(queue) >= 1
+
+
+N = int(input())
+QUEUE = []
+
+for _ in range(N):
+    line = sys.stdin.readline().split()
+    solution(QUEUE, line)
---
+++ algorythm/data_structure/baekjoon_1874.py
@@ -0,0 +1,32 @@
+# https://www.acmicpc.net/problem/1874
+# 스택 수열
+# TODO: Here !
+import sys
+
+
+def solution(n, seq):
+    current = 1
+    res = []
+    stack = []
+
+    for current_sequence in seq:  # 1 2 5 3 4
+        while current <= current_sequence and current <= n:
+            stack.append(current)
+            res.append("+")
+            current += 1
+
+        stack_number = stack.pop()
+
+        if stack_number != current_sequence:
+            return ["NO"]
+        else:
+            res.append("-")
+    return res
+
+
+N = int(sys.stdin.readline())
+sequence = list(int(sys.stdin.readline()) for _ in range(N))
+result = solution(N, sequence)
+
+for answer in result:
+    print(answer)
