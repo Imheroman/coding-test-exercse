050d29b2eb871201f64d9fdc9aadebf16bb0e62e
nyeong <duddnd4457@chosun.ac.kr>
2025-01-13 19:35:30 +0900

    백준 Data Structure 문제 업로드

--- algorythm/strings/baekjoon_1541.py
+++ algorythm/strings/baekjoon_1541.py
@@ -17,3 +17,45 @@ for index, letters in enumerate(expression):
         answer *= -1
 
 print(answer)
+
+## origin
+
+# # https://www.acmicpc.net/problem/1541
+# # 잃어버린 괄호
+# from sys import stdin
+# from collections import deque
+# input = stdin.readline
+#
+# expression = deque(stdin.readline())
+# numbers = deque()
+#
+# while expression:
+#     num = expression.popleft()
+#
+#     while expression and expression[0] != "-" and expression[0] != "+":
+#         num += expression.popleft()
+#
+#     numbers.append(int(num))
+#
+# answer = 0
+# stack = deque()
+#
+# while numbers:
+#     if numbers[0] < 0:
+#         answer += numbers.popleft()
+#
+#         while numbers and numbers[0] > 0:
+#             answer -= numbers.popleft()
+#     else:
+#         answer += numbers.popleft()
+# # for number in numbers:
+# #     if number < 0:
+# #         stack.append(-number)
+# #     else:
+# #         while stack:
+# #             answer -= stack.pop()
+# #         answer += number
+#
+# print(answer)
+#
+#
